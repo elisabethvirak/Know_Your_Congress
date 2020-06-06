c4824fa6c33112df2fade30b1ef26733999fc487
Elisabeth <elisabethvirak@gmail.com>
2020-06-06 14:02:08 -0400

    Added id to repname list

--- static/js/app.js
+++ static/js/app.js
@@ -17,23 +17,39 @@
             // console.log(firstName);
             var lastName = i.last_name;
             // console.log(lastName);
+            var repID = i.id;
+            // console.log(repID);
 
             // create string of representatives' entire name
             var repName = shortTitle + ` ` + firstName + ` ` + lastName;
             // add to repNameList to iterate through later
-            repNameList.push(repName)
+            repNameList.push(repID, repName)
             console.log(repNameList);
 
             // select dropdown menu
             var dropdownMenu = d3.select('#congressPerson');
             //iterate through representative names list to add choices to the dropdown menu
             repNameList.forEach(name => {
-                // console.log(i);
+                // console.log(name);
                 var menuOption = dropdownMenu.append('option');
-                // menuOption.attr('value', name);
+                menuOption.attr('value', name);
                 menuOption.text(name);
             });
         })
     })
+
+// // fill panel box with representative's info
+// function buildPlot(rep) {
+//     const dataFile = "/members";
+//     d3.json("/members").then(memberData => {
+//         // console.log(memberData);
+
+//         var repCard = d3.select('#rep-info')
+//         repCard.html("");
+//         //add row for each key:value pair
+//         Object.entries()
+//     });
+// }
+// buildPlot();
 // }
 // init();
