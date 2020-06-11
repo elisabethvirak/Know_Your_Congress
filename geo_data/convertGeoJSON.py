3882e40fb2b1448eda0cadc4ed0d537d47a661b6
Tyler Wiseman <wisemantyr@gmail.com>
2020-06-11 16:28:45 -0400

    added comments

--- geo_data/convertGeoJSON.py
+++ geo_data/convertGeoJSON.py
@@ -1,8 +1,10 @@
 import geopandas as gpd
 
+#use geopandas to create geopandas dataframe from zip file (downloaded from census.gov)
 zipfile = r"zip://tl_2019_us_cd116.zip"
-districts_gdf = gpd.read_file(zipfile) #use geopandas to create geopandas dataframe from zip file
+districts_gdf = gpd.read_file(zipfile)
 
-districts_gdf.to_file("../../Know_Your_Congress_extras/map/cd_116.geojson", driver='GeoJSON') #save dataframe as geoJSON
+#save dataframe as geoJSON
+districts_gdf.to_file("../../Know_Your_Congress_extras/map/cd_116.geojson", driver='GeoJSON') 
 
 #the geoJSON was loaded into Mapbox to create a tile layer. Styling for map done in Mapbox Studio.
--- static/js/congressDistrictsMap.js
+++ static/js/congressDistrictsMap.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = MAP_API;
 
 var map = new mapboxgl.Map({
   container: 'mapid', // HTML container ID
-  style: "mapbox://styles/wisemantyr/ckba88c190acp1io27sujasln", // style URL
+  style: "mapbox://styles/wisemantyr/ckba88c190acp1io27sujasln", // style URL (custom style made in mapbox studio)
   center: [-98.555183, 39.809860], // starting position as [lng, lat]
   zoom: 4
 });
