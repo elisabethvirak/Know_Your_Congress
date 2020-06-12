280618b616e8d5d4f5f8d521ea4db5fdf704049c
Elisabeth <elisabethvirak@gmail.com>
2020-06-11 21:22:09 -0400

    Fixed map

--- static/js/congressDistrictsMap.js
+++ static/js/congressDistrictsMap.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = MAP_API;
 
 var map = new mapboxgl.Map({
-  container: '#map', // HTML container ID
+  container: 'map', // HTML container ID
   style: "mapbox://styles/wisemantyr/ckba88c190acp1io27sujasln", // style URL (custom style made in mapbox studio)
   center: [-98.555183, 39.809860], // starting position as [lng, lat]
   zoom: 4
